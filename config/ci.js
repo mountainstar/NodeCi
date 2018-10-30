b18036d7ece82b822b05cc985dadd55cb1a41590
Chris Wirth <cwirth@env0365-3.local>
2018-10-29 21:12:58 -0600

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://chriswirth:Ph53n4x420@ds229290.mlab.com:29290/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:2717/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
